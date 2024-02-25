066b86c7de2d504f6fa0906821c3688bb548165f
Max BM <maxbuckmiremonro@gmail.com>
2024-02-25 00:31:20 +0000

    stow in stages

--- setup.sh
+++ setup.sh
@@ -23,6 +23,7 @@ fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Install zsh configuration
 stow zshrc
+source ~/.zshrc
 
 # --- INSTALL NERD FONT --- #
 stow fonts
@@ -31,10 +32,10 @@ fc-cache -fv
 # --- INSTALL ALACRITTY --- #
 # Ref: https://github.com/alacritty/alacritty/blob/master/INSTALL.md
 # Clone the source code
-git clone https://github.com/alacritty/alacritty.git
-cd alacritty
-# Install rustup.rs
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+git clone https://github.com/alacritty/alacritty.git alacritty-remote
+cd alacritty-remote
+# Install rust and cargo
+curl https://sh.rustup.rs -sSf | sh
 # Install ubuntu/debian dependencies
 if [[ "$OSTYPE" == "linux"* ]]; then
     sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
@@ -52,10 +53,12 @@ fi
 # Install terminfo
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
 # Add desktop entry
-sudo cp target/release/alacritty /usr/local/bin
-sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-sudo desktop-file-install extra/linux/Alacritty.desktop
-sudo update-desktop-database
+if [[ "$OSTYPE" == "linux"* ]]; then
+    sudo cp target/release/alacritty /usr/local/bin
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+fi
 # Install man pages
 sudo mkdir -p /usr/local/share/man/man1
 sudo mkdir -p /usr/local/share/man/man5
@@ -64,8 +67,7 @@ scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/
 scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
 scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
 # Install (zsh) shell completions
-mkdir -p ${ZDOTDIR:-~}/.zsh_functions
-echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
 cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
 # Install alacritty configuration
+cd ~/.dotfiles
 stow alacritty
--- zshrc/.zshrc
+++ zshrc/.zshrc
@@ -1,6 +1,7 @@
-export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:$HOME/.cargo/bin:/usr/local/bin:$PATH
 eval "$(/opt/homebrew/bin/brew shellenv)"
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME=""
 plugins=(git)
 source $ZSH/oh-my-zsh.sh
+fpath+=${ZDOTDIR:-~}/.zsh_functions
